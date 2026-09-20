6895511830f53ff363bf4ee77796ed1ac5a40b74
Tanishq <slatorgator3@gmail.com>
2026-09-20 14:14:18 +0530

    Trebell desktop: switch preload to sandbox-compatible CommonJS

--- trebell-code/desktop/preload.mjs
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-
-contextBridge.exposeInMainWorld("trebellDesktop", {
-  minimize:()=>ipcRenderer.send("window:minimize"),
-  maximize:()=>ipcRenderer.send("window:maximize"),
-  close:()=>ipcRenderer.send("window:close"),
-  platform:process.platform,
-});
